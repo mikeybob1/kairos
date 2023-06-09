2fa6fc9d23c4e5c241feb159bb01098d509bd25c
Auto Sync <noreply@ji-sang.com>
2023-06-09 08:37:26 +0000

    add readdir func & change the cc compiler flags for user programs

--- user/lib/stdio.c
+++ user/lib/stdio.c
@@ -27,98 +27,103 @@ int puts(const char *s)
     return r;
 }
 
-static void out(int f, const char *s, size_t l)
-{
-    write(f, s, l);
+/* used by printf */
+void _putchar(char c) {
+    putchar(c);
 }
 
-static char digits[] = "0123456789abcdef";
+// static void out(int f, const char *s, size_t l)
+// {
+//     write(f, s, l);
+// }
 
-static void printint(int xx, int base, int sign)
-{
-    char buf[16 + 1];
-    int i;
-    uint x;
+// static char digits[] = "0123456789abcdef";
 
-    if (sign && (sign = xx < 0))
-        x = -xx;
-    else
-        x = xx;
+// static void printint(int xx, int base, int sign)
+// {
+//     char buf[16 + 1];
+//     int i;
+//     uint x;
 
-    buf[16] = 0;
-    i = 15;
-    do
-    {
-        buf[i--] = digits[x % base];
-    } while ((x /= base) != 0);
+//     if (sign && (sign = xx < 0))
+//         x = -xx;
+//     else
+//         x = xx;
 
-    if (sign)
-        buf[i--] = '-';
-    i++;
-    if (i < 0)
-        puts("printint error");
-    out(stdout, buf + i, 16 - i);
-}
+//     buf[16] = 0;
+//     i = 15;
+//     do
+//     {
+//         buf[i--] = digits[x % base];
+//     } while ((x /= base) != 0);
 
-static void printptr(uint64_t x)
-{
-    int i = 0, j;
-    char buf[32 + 1];
-    buf[i++] = '0';
-    buf[i++] = 'x';
-    for (j = 0; j < (sizeof(uint64_t) * 2); j++, x <<= 4)
-        buf[i++] = digits[x >> (sizeof(uint64_t) * 8 - 4)];
-    buf[i] = 0;
-    out(stdout, buf, i);
-}
+//     if (sign)
+//         buf[i--] = '-';
+//     i++;
+//     if (i < 0)
+//         puts("printint error");
+//     out(stdout, buf + i, 16 - i);
+// }
+
+// static void printptr(uint64_t x)
+// {
+//     int i = 0, j;
+//     char buf[32 + 1];
+//     buf[i++] = '0';
+//     buf[i++] = 'x';
+//     for (j = 0; j < (sizeof(uint64_t) * 2); j++, x <<= 4)
+//         buf[i++] = digits[x >> (sizeof(uint64_t) * 8 - 4)];
+//     buf[i] = 0;
+//     out(stdout, buf, i);
+// }
 
 // Print to the console. only understands %d, %x, %p, %s.
-void printf(const char *fmt, ...)
-{
-    va_list ap;
-    int l = 0;
-    char *a, *z, *s = (char *)fmt;
-    int f = stdout;
+// void printf(const char *fmt, ...)
+// {
+//     va_list ap;
+//     int l = 0;
+//     char *a, *z, *s = (char *)fmt;
+//     int f = stdout;
 
-    va_start(ap, fmt);
-    for (;;)
-    {
-        if (!*s)
-            break;
-        for (a = s; *s && *s != '%'; s++)
-            ;
-        for (z = s; s[0] == '%' && s[1] == '%'; z++, s += 2)
-            ;
-        l = z - a;
-        out(f, a, l);
-        if (l)
-            continue;
-        if (s[1] == 0)
-            break;
-        switch (s[1])
-        {
-        case 'd':
-            printint(va_arg(ap, int), 10, 1);
-            break;
-        case 'x':
-            printint(va_arg(ap, int), 16, 1);
-            break;
-        case 'p':
-            printptr(va_arg(ap, uint64_t));
-            break;
-        case 's':
-            if ((a = va_arg(ap, char *)) == 0)
-                a = "(null)";
-            l = strnlen(a, 200);
-            out(f, a, l);
-            break;
-        default:
-            // Print unknown % sequence to draw attention.
-            putchar('%');
-            putchar(s[1]);
-            break;
-        }
-        s += 2;
-    }
-    va_end(ap);
-}
+//     va_start(ap, fmt);
+//     for (;;)
+//     {
+//         if (!*s)
+//             break;
+//         for (a = s; *s && *s != '%'; s++)
+//             ;
+//         for (z = s; s[0] == '%' && s[1] == '%'; z++, s += 2)
+//             ;
+//         l = z - a;
+//         out(f, a, l);
+//         if (l)
+//             continue;
+//         if (s[1] == 0)
+//             break;
+//         switch (s[1])
+//         {
+//         case 'd':
+//             printint(va_arg(ap, int), 10, 1);
+//             break;
+//         case 'x':
+//             printint(va_arg(ap, int), 16, 1);
+//             break;
+//         case 'p':
+//             printptr(va_arg(ap, uint64_t));
+//             break;
+//         case 's':
+//             if ((a = va_arg(ap, char *)) == 0)
+//                 a = "(null)";
+//             l = strnlen(a, 200);
+//             out(f, a, l);
+//             break;
+//         default:
+//             // Print unknown % sequence to draw attention.
+//             putchar('%');
+//             putchar(s[1]);
+//             break;
+//         }
+//         s += 2;
+//     }
+//     va_end(ap);
+// }
